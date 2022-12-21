11d44390d06d4d2b3537a86dc097e8492b04bf7a
katibu00 <ukmisau@gmail.com>
2022-12-21 07:53:30 +0100

    login form

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
+<script setup>
+  import { reactive } from 'vue';
+  import axios from 'axios';
+
+  let form = reactive({
+    email: '',
+    password: '',
+  });
+
+  const login = async()=>{
+    await axios.post('http://localhost:8000/api/login', form)
+    .then(res=>{
+      console.log(res);
+    })
+  }
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <form @submit.prevent="login">
+      <input type="email" id="email" v-model="form.email" placeholder="Enter Email" />
+      <input type="password" id="password" v-model="form.password" placeholder="****" />
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
